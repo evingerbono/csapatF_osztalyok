0ccf918c2fde3c02cee5b5ef704a227d85349fe0
evingerbono <bono0823@gmail.com>
2023-10-09 08:41:55 +0200

    NegyzetOsztaly

--- CsapatFeladat/src/csapatfeladat/Negyzet.java
+++ CsapatFeladat/src/csapatfeladat/Negyzet.java
@@ -2,4 +2,31 @@ package csapatfeladat;
 
 public class Negyzet {
     private int a;
+    private int x,y;
+    
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+
+    public void setA(int a) {
+        if (a<1) {
+            a=2;
+        }
+        this.a = a;
+    }
+    
+    public int terulet(int a){
+        this.a=a;
+        int teruelt=a*a;
+        return teruelt;
+    }
+    
+    public void kordinata(int x, int y){
+        this.x=x;
+        this.y=y;
+    }
 }
