4b001068579d62b97c544820e8f7dc8b5869d9fa
fureszb <fureszb@gmail.com>
2023-10-09 08:31:00 +0200

    negyzetFeladat

--- CsapatFeladat/src/csapatfeladat/Negyzet.java
+++ CsapatFeladat/src/csapatfeladat/Negyzet.java
@@ -1,5 +1,15 @@
 package csapatfeladat;
 
 public class Negyzet {
+
     private int a;
+    private int x;
+    private int y;
+
+   public String Kiiratas(){
+       return "X kordináta: "+x+"\nY kordináta: "+y+"\n\tA négyzet területe: "+a*a;
+   }
+   
+   
+
 }
---
+++ CsapatFeladat/src/csapatfeladat/main.java
@@ -0,0 +1,13 @@
+
+package csapatfeladat;
+
+
+public class main {
+
+   
+    public static void main(String[] args) {
+        System.out.println(new Negyzet().Kiiratas());
+       
+    }
+    
+}
